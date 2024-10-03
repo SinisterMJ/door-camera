7888b20a486fd07b3819426cfae2b95c85b1df76
Anton Roth <Anton_Roth@gmx.de>
2024-10-03 21:04:55 +0200

    Initial check. Capturing via RTSP worked

---
+++ src/cameracapture.py
@@ -0,0 +1,22 @@
+import cv2
+from subprocess import Popen
+
+class CameraCapture:
+    def __init__(self):
+        self.neolink = Popen(['/home/roth/development/door-camera/bin/neolink', 'rtsp', '--config=/home/roth/development/door-camera/bin/neolink.toml'])
+        self.cv_capture = cv2.VideoCapture("rtsp://127.0.0.1:8554/Camera01/Main", cv2.CAP_FFMPEG)
+        pass
+        
+
+    def retrieveImage(self):
+        ret, frame = self.cv_capture.read()
+
+        if ret:
+            return frame
+        else:
+            return None
+
+
+    def __del__(self):
+        self.neolink.terminate()
+        pass
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from cameracapture import CameraCapture
+import time
+
+t = CameraCapture()
+
+
+
+# print("CameraCapture available")
+# time.sleep(300)
+# print("CameraCapture no longer available")
